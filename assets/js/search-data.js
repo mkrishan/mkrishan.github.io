3c1828c3bae1e3c0135a2e815dfc288bdda870db
mkrishan <manish.makes@gmail.com>
2025-12-24 15:12:18 +0000

    Deploying to gh-pages from @ mkrishan/mkrishan.github.io@95f9747066ccec86ebdc2c06937581eadf6c3d8c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Previous teaching and supervision.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
+          },
+        },{id: "nav-publications",
           title: "publications",
           description: "",
           section: "Navigation",
